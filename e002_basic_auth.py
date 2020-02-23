5f46c8e77e5d6d3fa2d52d9011d90295f2962234
Hector Vega <linnk99@gmail.com>
2020-02-23 13:15:42 -0500

    Basic structure for excercise 002 made

---
+++ e002_basic_auth.py
@@ -0,0 +1,21 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+class UsingUnittest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Opera(executable_path = "./operadriver")
+    
+    def test_basic_auth(self):
+        driver = self.driver
+        driver.get("http://the-internet.herokuapp.com")
+
+    def tearDown(self):
+        print("The browser is about to close...")
+        sleep(3)
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
