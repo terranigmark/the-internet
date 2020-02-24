c0d56123bfdc9180cbb4dcae2ab96dfcc97c8b79
Hector Vega <linnk99@gmail.com>
2020-02-23 20:19:11 -0500

    Excercise finished, pointing if the checkbox is checked or not after clicking on it

--- e003_checkboxes.py
+++ e003_checkboxes.py
@@ -14,8 +14,11 @@ class UsingUnittest(unittest.TestCase):
         excercise.click()
         sleep(1)
 
-        checkbox = driver.find_elements_by_css_selector(input.type)
-        checkbox.click()
+        for i in range (2):
+            checkbox = driver.find_element_by_xpath(f'/html/body/div[2]/div/div/form/input[{i + 1}]')
+            if checkbox.is_selected():
+                print(f'The checkbox number {i + 1} is selected')
+            checkbox.click()
 
     def tearDown(self):
         print("Browser is about to close...")
