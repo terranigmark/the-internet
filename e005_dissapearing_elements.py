2a1505df952489083551411d2060550719c1041b
Hector Vega <linnk99@gmail.com>
2020-02-24 22:47:19 -0500

    Excercise finished and working, it shows the number of tries

--- e005_dissapearing_elements.py
+++ e005_dissapearing_elements.py
@@ -12,7 +12,25 @@ class UsingUnittest(unittest.TestCase):
         driver.get("http://the-internet.herokuapp.com")
         excercise = driver.find_element_by_link_text("Disappearing Elements")
         excercise.click()
+
+        options = []
+        menu = 5
+        tries = 1
+
+        while len(options) < 5:
+            options.clear()
+
+            for i in range(menu):
+                try:
+                    option_name = driver.find_element_by_xpath(f"/html/body/div[2]/div/div/ul/li[{i + 1}]/a")
+                    options.append(option_name.text)
+                    print(options)
+                except:
+                    print(f"Option number {i + 1} is NOT FOUND")
+                    tries += 1
+                    driver.refresh()
         
+        print(f"Finished in {tries} tries")
 
     def tearDown(self):
         print("Browser is about to close...")
