79f44f975c148847affe6a5138709c19c70df6b1
Hector Vega <linnk99@gmail.com>
2020-02-27 22:31:18 -0500

    Comments for excecise 007 added

--- e007_dropdown.py
+++ e007_dropdown.py
@@ -1,3 +1,10 @@
+###HANDLING DROPDOWN MENUS
+'''
+This script goes to http://the-internet.herokuapp.com/dropdown
+Focuses on the dropdown menu and select each option available
+This is done by different methods and can be done with control flows too
+'''
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
@@ -5,17 +12,22 @@ from time import sleep
 
 class UsingUnittest(unittest.TestCase):
 
+    #Setup browser instance using Opera
     def setUp(self):
         self.driver = webdriver.Opera(executable_path = "./operadriver")
 
+    #Create browser instance and go to the excercise website
     def test_handling_dropdown(self):
         driver = self.driver
         driver.get("http://the-internet.herokuapp.com")
         excercise = driver.find_element_by_link_text("Dropdown")
         excercise.click()
 
+        #Creates and instance that focus on the dropdown menu
         dropdown_menu = Select(driver.find_element_by_id("dropdown"))
         sleep(1)
+        
+        #Select dropdown's options through different methods
         dropdown_menu.select_by_value("1")
         sleep(1)
         dropdown_menu.select_by_value("2")
@@ -28,6 +40,7 @@ class UsingUnittest(unittest.TestCase):
         sleep(1)
         dropdown_menu.select_by_visible_text("Option 2")
 
+    #Tearing down browser's instance
     def tearDown(self):
         print("Browser is about to close...")
         sleep(1)
