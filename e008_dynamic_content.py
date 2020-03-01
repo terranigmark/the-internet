3ab8cce53850de4657b5bea6112a8d320b7b662b
Hector Vega <linnk99@gmail.com>
2020-02-29 20:49:18 -0500

    Script of excercise 008 modified to catch all different commets, found it was a total of 15

--- e008_dynamic_content.py
+++ e008_dynamic_content.py
@@ -14,11 +14,14 @@ class UsingUnittest(unittest.TestCase):
         excercise.click()
 
         pictures = []
+        posts = []
         total_pictures = 5
+        total_comments = 0
         shown_pictures = 3
+        shown_comments = 3
         tries = 1
 
-        while len(pictures) < total_pictures:
+        while len(pictures) < total_pictures and len(posts) < 30:
 
             for i in range(shown_pictures):
                 avatar = driver.find_element_by_xpath(f"/html/body/div[2]/div/div/div/div/div[{i + 1}]/div[1]/img")
@@ -26,11 +29,28 @@ class UsingUnittest(unittest.TestCase):
                 
                 if avatar_route not in pictures:
                     pictures.append(avatar_route)
-                    print(pictures)                
+                    
+                else:
+                    pass
+            
+            for i in range(shown_comments):
+                post = driver.find_element_by_xpath(f"/html/body/div[2]/div/div/div/div/div[{i + 1}]/div[2]")
+                post_test = post.text
+                print(post_test)
+                total_comments += 1
+
+                if post_test not in posts:
+                    posts.append(post_test)                    
+                else:
+                    print("There are no new posts")
                 
             tries += 1
-            print(tries)
+            print(pictures)
+            print(f"\n COMENT BATCH #{len(posts)}   ".join(posts))
+            print(tries, total_pictures, total_comments)
             driver.refresh()            
+
+            print(posts[::-1])
 
     def tearDown(self):
         print("Browser is about to close...")
@@ -41,7 +61,13 @@ if __name__ == "__main__":
     unittest.main()
 
 """ 
+Pictures XPath structure
 /html/body/div[2]/div/div/div/div/div[1]/div[1]/img
 /html/body/div[2]/div/div/div/div/div[2]/div[1]/img
 /html/body/div[2]/div/div/div/div/div[3]/div[1]/img 
+
+Text XPath structure
+/html/body/div[2]/div/div/div/div/div[1]/div[2]
+/html/body/div[2]/div/div/div/div/div[2]/div[2]
+/html/body/div[2]/div/div/div/div/div[3]/div[2]/text()
 """
