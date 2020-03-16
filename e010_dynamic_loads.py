0955ed4237fe615354668ae5cd937eddb13ff1b3
Hector Vega <linnk99@gmail.com>
2020-03-15 23:25:32 -0500

    Unable to detect the h4 tag after it appears, will try with an implicit await

--- e010_dynamic_loads.py
+++ e010_dynamic_loads.py
@@ -16,15 +16,20 @@ class UsingUnittest(unittest.TestCase):
         excercise = driver.find_element_by_link_text("Dynamic Loading")
         excercise.click()
 
-        hiddent_content_test = driver.find_element_by_link_text("Example 1: Element on page that is hidden")
-        hiddent_content_test.click()
+        hidden_content_test = driver.find_element_by_link_text("Example 1: Element on page that is hidden")
+        hidden_content_test.click()
 
         start_btn = driver.find_element_by_tag_name("button")
         start_btn.click()
+
+        hidden_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*div[@id='finish']")))
+
+        hidden_text = driver.find_element_by_xpath('//*[@id="finish"]/h4')
+        print(f'The hiddent text is: {hidden_text.text}')
         
 
     def test_rendered_content(self):
-        driver = self.driver
+        pass
 
     def tearDown(self):
         print('Browser is about to close...')
