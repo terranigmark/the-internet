64f4f8509df5e37596ba91230b5b86d9a71e428c
Hector Vega <linnk99@gmail.com>
2020-03-15 22:32:27 -0500

    Comments added to excercise 009

--- e009_dynamic_controls.py
+++ e009_dynamic_controls.py
@@ -1,8 +1,8 @@
 ###HANDLING DYNAMIC CONTROLS
 '''
-This script goes to http://the-internet.herokuapp.com/dropdown
-Focuses on the dropdown menu and select each option available
-This is done by different methods and can be done with control flows too
+This script goes to http://the-internet.herokuapp.com/dynamic_controls
+Where there are two controsl which take some time to be active/inactive
+In order to use them after being activate it can be used some kind of wait
 '''
 
 import unittest
@@ -12,20 +12,24 @@ from time import sleep
 
 class UsingUnittest(unittest.TestCase):
 
+    #Setup browser instance using Opera
     def setUp(self):
         self.driver = webdriver.Opera(executable_path = "./operadriver")
 
+    #Create browser instance and go to the excercise website
     def test_using_dynamic_controls(self):
         driver = self.driver
         driver.get("http://the-internet.herokuapp.com")
         excercise = driver.find_element_by_link_text("Dynamic Controls")
         excercise.click()
 
+        #Identifies the checkbox, clicks on it and then in the 'remove' button
         checkbox = driver.find_element_by_xpath('//input[@type="checkbox"]')
         checkbox.click()
         remove_checkbox_btn = driver.find_element_by_xpath('//*[@id="checkbox-example"]/button')
         remove_checkbox_btn.click()
 
+        #Identifies the 'enable' button, clicks on it, focus the textbox and inputs a text
         enable_btn = driver.find_element_by_xpath('//*[@id="input-example"]/button')
         enable_btn.click()
         sleep(5)
@@ -33,7 +37,7 @@ class UsingUnittest(unittest.TestCase):
         textbox.send_keys("ola ke ase")
         enable_btn.click()
 
-
+    #Tearing down browser's instance
     def tearDown(self):
         print('Browser is about to close...')
         sleep(3)
